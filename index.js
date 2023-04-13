50db48453429de09e1ce742a14e3427a6efc491a
xiawang1024 <xiawang1024@gmail.com>
2023-04-13 15:08:26 +0800

    ✨ feat: add dog

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: xiawang1024
  * @Date: 2023-04-13 14:58:06
- * @LastEditTime: 2023-04-13 15:02:54
+ * @LastEditTime: 2023-04-13 15:08:13
  * @LastEditors: xiawang1024
  * @Description: 
  * @FilePath: /gitflow-test/index.js
@@ -15,3 +15,5 @@ class Animal {
         console.log(`${this.name} is can eat`);
     }
 }
+
+let dog = new Animal('dog');
